7f63aa779e858a1428f4ea70548c260752f54e61
Raptor8532 <23257517+Raptor8532@users.noreply.github.com>
2023-12-04 17:56:21 +0100

    adding a VM

--- virtual-network/main.tf
+++ virtual-network/main.tf
@@ -104,3 +104,31 @@ resource "azurerm_network_interface" "abc-nic" {
   }
 }
 
+resource "azurerm_linux_virtual_machine" "abc-vm" {
+  name                = "abc-vm"
+  resource_group_name = azurerm_resource_group.abc-rg.name
+  location            = azurerm_resource_group.abc-rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.abc-nic.id,
+  ]
+  custom_data = filebase64("${path.module}/customdata.tpl")
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("${path.module}/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
