3c83e19cb00793c2ee17746853f041c240c7f4bd
Raptor8532 <23257517+Raptor8532@users.noreply.github.com>
2023-12-04 15:35:07 +0100

    creation of rg, vn and subnet

--- virtual-network/main.tf
+++ virtual-network/main.tf
@@ -21,3 +21,31 @@ provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
+
+resource "azurerm_resource_group" "abc-rg" {
+  name     = "abc-resources"
+  location = "West Europe"
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_virtual_network" "abc-vn" {
+  name                = "abc-network"
+  resource_group_name = azurerm_resource_group.abc-rg.name
+  location            = azurerm_resource_group.abc-rg.location
+  address_space       = ["10.123.0.0/16"]
+
+  tags = {
+    environment = "dev"
+  }
+
+}
+
+resource "azurerm_subnet" "abc-subnet" {
+  name                 = "abc-subnet1"
+  resource_group_name  = azurerm_resource_group.abc-rg.name
+  virtual_network_name = azurerm_virtual_network.abc-vn.name
+  address_prefixes    = ["10.123.1.0/24"]
+
+}
